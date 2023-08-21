b5324153edc2fdc4babfd6ea7be7169f1b3e3e75
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-08-22 00:19:33 +0530

    Had a glance of Inheritance using constructive way

--- call,apply,bind/ex03.js
+++ call,apply,bind/ex03.js
@@ -10,5 +10,24 @@ const contact = {
   phone: "123-456-7890",
 };
 // printInfo.call(null, Object.values(contact)); //Parameters will assume 2nd arg as single value.
-printInfo.call(null, ...Object.values(contact));
+// printInfo.call(null, ...Object.values(contact));
 // Here it is important to use rest opereator else parameters will consider 2nd argu as single value.
+
+//Q2
+function Parent(name) {
+  this.name = name;
+  this.gender = "Male";
+  this.Parent_hobby = "Teaching";
+}
+
+function Child(name, age) {
+  Parent.call(this, name); // Here we are calling Parent fn with reference of Child. All properties of Praent would be accessible for child.
+  this.age = age;
+  this.gender = this.gender;
+  this.Parent_hobby = this.Parent_hobby;
+}
+
+const child = new Child("Charlie", 5);
+console.log(child.name);
+console.log(child.gender);
+console.log(child.Parent_hobby);
