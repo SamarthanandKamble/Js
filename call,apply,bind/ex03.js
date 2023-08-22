2cd6dfc379631ba0be413fe47dec25a41d30d8eb
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-08-22 23:06:35 +0530

    practice on call method in javascript

--- call,apply,bind/ex03.js
+++ call,apply,bind/ex03.js
@@ -47,4 +47,113 @@ var operation = {
   },
 };
 
-calculate.call(operation, ...Object.values(operation));
+// calculate.call(operation, ...Object.values(operation));
+
+//Q5 How can you use the `call` method to greet both users with the message "Hi"?
+function greet(message) {
+  console.log(`${message}, ${this.name}!`);
+}
+
+const user1 = { name: "Alex" };
+const user2 = { name: "Eva" };
+
+// greet.call(user1, "Hi");
+// greet.call(user2, "Hi");
+
+//Q6 How can you use the call method to make the alien object use the sayHello method from the person object?
+const personn = {
+  name: "Alice",
+  sayHello: function () {
+    console.log(`Hello, my name is ${this.name}.`);
+  },
+};
+
+const alien = {
+  name: "Zog",
+  age: 100,
+};
+
+// personn.sayHello.call(alien);
+
+//Q7
+function Animal(name) {
+  this.name = name;
+}
+
+function Dog(name, breed) {
+  Animal.call(this, name);
+  this.breed = breed;
+}
+
+// const myDog = new Dog("Buddy", "Golden Retriever");
+// console.log(myDog.name); // What will this output?
+// console.log(myDog.breed);
+
+//Q8 Create two objects, event1 and event2, each with a different message property. Use the call method to make the announce function announce the messages for both events.
+function announce(message) {
+  console.log(message);
+}
+var event1 = {
+  message: "Hey, this is event 1",
+};
+var event2 = {
+  message: "Hey, this is event 2",
+};
+// announce.call(null, ...Object.values(event1));
+// announce.call(null, ...Object.values(event2));
+
+//Q9 Create an array of objects, each having a name property. Use a loop and the call method to greet each person with a different message.
+function greett(message) {
+  console.log(`${message}, ${this.name}!`);
+}
+
+var person = [
+  {
+    message: "hey there, this is object of person1",
+    name: "Samarth",
+  },
+  {
+    message: "hey there, this is object of person2",
+    name: "Samarthanand",
+  },
+  {
+    message: "hey there, this is object of person3",
+    name: "Kamble",
+  },
+  {
+    message: "hey there, this is object of person4",
+    name: "Samarthanand Kamble",
+  },
+];
+
+// for (const iterator of person) {
+//   greett.call(iterator, ...Object.values(iterator));
+// }
+
+//Q10 Write a function called introduce that accepts a greeting as an argument and uses the call method to print a full introduction (including first and last name). Invoke the function with the person object and a greeting of your choice.
+
+function introduce(greeting) {
+  console.log(`${greeting} , ${this.firstName} ${this.lastName}`);
+}
+
+const peerson = {
+  firstName: "Alice",
+  lastName: "Johnson",
+};
+
+// introduce.call(peerson, "Hey");
+
+//Q10 Create an object named logger with a message property. Write a method named logData that uses the call method to pass the message property to the callback function of fetchData. Call the logData method.
+function fetchData(callback) {
+  const data = "Fetched data";
+  callback(data);
+}
+
+var logger = {
+  message: "Hey",
+  logData() {
+    fetchData(this.message);
+  },
+};
+
+logger.logData.call(logger, logger.logData);
