337841b1303ee53934326b1ff353020474390382
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-08-30 00:34:28 +0530

    Learned about Pyramid of doom

--- callbackfn/ex_02.js
+++ callbackfn/ex_02.js
@@ -11,35 +11,44 @@
 */
 /* Here we neeed to make sure, no change in the order of execution of steps while making tea*/
 
-function boilTheWater(cb) {
-  setTimeout(() => {
-    console.log("boilTheWater");
-    cb();
-  }, 3000);
+function boilTheWater() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("boilTheWater executed");
+      console.log("boilTheWater");
+    }, 3000);
+  });
 }
 
-function addTeaLeaves(cb) {
-  setTimeout(() => {
-    console.log("addTeaLeaves");
-    cb();
-  }, 1000);
+function addTeaLeaves() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+      console.log("addTeaLeaves");
+    }, 1000);
+  });
 }
-function addSugar(cb) {
-  setTimeout(() => {
-    console.log("addSugar");
-    cb();
-  }, 1000);
+function addSugar() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+      console.log("addSugar");
+    }, 5000);
+  });
 }
-function addMilk(cb) {
-  setTimeout(() => {
-    console.log("addMilk");
-    cb();
-  }, 1000);
+function addMilk() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+      console.log("addMilk");
+    }, 1000);
+  });
 }
 function pourTheTea() {
   console.log("pourTheTea");
 }
 
+/*
 boilTheWater(function () {
   addTeaLeaves(function () {
     addSugar(function () {
@@ -49,5 +58,29 @@ boilTheWater(function () {
     });
   });
 });
+*/
+// this is know as PYRAMID OF DOOM. (nested functions)
+
+// Lets try to make this more simler and use Promises.
+
+boilTheWater()
+  .then(() => addTeaLeaves())
+  .then(() => addSugar())
+  .then(() => addMilk())
+  .then(() => pourTheTea());
+
+
+
+//Here is the async await way which works similar to promise.
+/*async function makeMilkTea() {
+  await boilTheWater();
+  await addTeaLeaves();
+  await addSugar();
+  await addMilk();
+  await pourTheTea();
+}
+
+makeMilkTea();
+*/
 
 console.log("Hey, we are making milk tea");
