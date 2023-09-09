df86d3aeab5b7d61351bc24375c61027cecc6c38
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-09-09 17:37:01 +0530

    Created container for card and added styling

--- DOM/ex03/script.js
+++ DOM/ex03/script.js
@@ -1,20 +1,33 @@
 const cntr = document.querySelector("#container");
 const heading3 = document.createElement("h3");
+const cardContainer = document.createElement("div");
+
 function createCard(data) {
+  cardContainer.style.display = "flex";
+  cardContainer.style.height = "100%";
+  cardContainer.style.width = "100%";
+  cardContainer.style.flexWrap = "wrap";
+  cardContainer.style.justifyContent = "space-around";
+  cardContainer.style.alignItems = "center";
+  cardContainer.style.alignContent = "space-around";
+  cardContainer.style.flexDirection = "row";
+
+  cntr.appendChild(cardContainer);
+
   data.forEach((element) => {
     var div = document.createElement("div");
-    cntr.appendChild(div);
-    div.style.display = "flex";
-    div.style.flexDirection = "row";
     div.style.height = "200px";
     div.style.width = "200px";
-    div.style.border = "2px solid #000";
+    div.style.margin = "10px";
+    div.style.padding = "10px";
+    div.style.border = "1px solid #000";
     const p1 = document.createElement("p");
     const p2 = document.createElement("p");
     const p3 = document.createElement("p");
-    p1.innerHTML = element.username;
-    p2.innerHTML = element.name;
-    p3.innerHTML = element.website;
+    cardContainer.appendChild(div);
+    p1.innerHTML = `Username : ${element.username}`;
+    p2.innerHTML = `Name : ${element.name}`;
+    p3.innerHTML = `Website : ${element.website}`;
     div.appendChild(p1);
     div.appendChild(p2);
     div.appendChild(p3);
