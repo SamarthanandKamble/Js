73f1f88d4d2183b856ea0a2f46571d43067db579
Samarth1002 <samarthanandkamble1002@gmail.com>
2023-09-16 22:37:46 +0530

    Added functionalities for Update the existing task

--- DOM/ex05/script.js
+++ DOM/ex05/script.js
@@ -20,12 +20,12 @@ function createContainer() {
   inputBox.style.fontSize = "800";
   searchDiv.appendChild(inputBox);
 
+  searchBtn.type = "submit";
   searchBtn.style.height = "10vh";
   searchBtn.style.width = "20%";
   searchBtn.textContent = "Search";
   searchDiv.appendChild(searchBtn);
 
-  taskDiv.classList.add = "taskListDiv";
   taskDiv.style.border = "2px dotted red";
   taskDiv.style.height = "60vh";
   taskDiv.style.width = "100%";
@@ -49,14 +49,20 @@ function addTaskList(task) {
   li.classList.add("task");
   let taskDoneBtn = document.createElement("button");
   let taskEditBtn = document.createElement("button");
-  taskDoneBtn.classList.add = "taskListDoneBtn";
-  taskEditBtn.classList.add = "taskListEditBtn";
-  taskDoneBtn.textContent = "done";
-  taskEditBtn.textContent = "edit";
-  li.appendChild(taskEditBtn);
-  li.appendChild(taskDoneBtn);
-  ul.appendChild(li);
+  taskDoneBtn.classList.add("taskListDoneBtn");
+  taskEditBtn.classList.add("taskListEditBtn");
+  taskDoneBtn.textContent = "Done";
+  taskEditBtn.textContent = "Edit";
+  let taskListDiv = document.createElement("div");
+  taskListDiv.classList.add("taskListdiv");
+  taskListDiv.style.display = "flex";
+  taskListDiv.appendChild(li);
+  taskListDiv.appendChild(taskEditBtn);
+  taskListDiv.appendChild(taskDoneBtn);
+  ul.appendChild(taskListDiv);
   taskDiv.appendChild(ul);
+  li.insertAdjacentElement("afterend", taskEditBtn);
+  li.insertAdjacentElement("afterend", taskDoneBtn);
 
   ul.style.width = "100%";
   li.style.textAlign = "start";
@@ -67,9 +73,25 @@ function addTaskList(task) {
   taskDoneBtn.style.margin = "2px";
   taskEditBtn.style.margin = "2px";
 
-  taskDoneBtn.addEventListener("click", () => markTodoDone(li));
+  taskDoneBtn.addEventListener("click", () => markTodoDone(taskListDiv));
+  taskEditBtn.addEventListener("click", () => editTheTask(taskListDiv));
 }
 
 function markTodoDone(item) {
-  item.style.textDecoration = "line-through";
+  item.remove();
+}
+
+function editTheTask(itemDiv) {
+  itemDiv.children[2].textContent = "Update";
+  let editTaskInputBox = document.createElement("input");
+  itemDiv.children[0].appendChild(editTaskInputBox);
+  // itemDiv.children[0].textContent = "";
+  itemDiv.children[2].addEventListener("click", function (e) {
+    console.log(e.target.value);
+    let updatedtask = editTaskInputBox.value;
+    console.log(updatedtask);
+    itemDiv.firstChild.textContent = updatedtask;
+    itemDiv.firstChild.style.display = "block";
+    itemDiv.children[2].textContent = "Edit";
+  });
 }
